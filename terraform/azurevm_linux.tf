8e3f547c433eb1fc609067ad303bbf02a7714814
srijaallam <srija.allam10@gmail.com>
2024-02-22 18:35:20 -0500

    tf code

--- terraform/azurevm_linux.tf
+++ terraform/azurevm_linux.tf
@@ -1,9 +1,16 @@
+locals{
+  vm_name = toset([
+    "master",
+    "worker"
+  ])
+}
+
 data "azurerm_resource_group" "resourcegroup" {
   name     = "${var.username}-k8s101-workshop"
 }
 
 resource "azurerm_virtual_network" "linuxvmnetwork" {
-  name                = "linuxvm_network"
+  name                = "${var.username}-k8s_network"
   address_space       = ["10.0.0.0/24"]
   location            = data.azurerm_resource_group.resourcegroup.location
   resource_group_name = data.azurerm_resource_group.resourcegroup.name
@@ -17,31 +24,31 @@ resource "azurerm_subnet" "protectedsubnet" {
 }
 
 resource "azurerm_public_ip" "linuxpip" {
-  count               = 2
-  name                = "linuxvm-${count.index}"
+  for_each            = local.vm_name
+  name                = "${each.key}_pip"
   location            = data.azurerm_resource_group.resourcegroup.location
   resource_group_name = data.azurerm_resource_group.resourcegroup.name
   allocation_method   = "Static"
-  domain_name_label   = "${var.username}-linuxvm-${count.index}"
+  domain_name_label   = "${var.username}-${each.key}"
 }
 
 resource "azurerm_network_interface" "nic" {
-  count               = 2
-  name                = "linuxvmnic-${count.index}"
+  for_each            = local.vm_name
+  name                = "${each.key}-node_nic"
   location            = data.azurerm_resource_group.resourcegroup.location
   resource_group_name = data.azurerm_resource_group.resourcegroup.name
 
   ip_configuration {
-    name                          = "linuxvm-nic-ipconfig-${count.index}"
+    name                          = "${each.key}_ipconfig"
     subnet_id                     = azurerm_subnet.protectedsubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.linuxpip[count.index].id
+    public_ip_address_id          = azurerm_public_ip.linuxpip[each.key].id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "linuxvm" {
-  count                 = 2
-  name                  = "linuxvm-${count.index}"
+  for_each              = local.vm_name
+  name                  = "node-${each.key}"
   resource_group_name   = data.azurerm_resource_group.resourcegroup.name
   location              = data.azurerm_resource_group.resourcegroup.location
   size                  = "Standard_B2s"
@@ -49,7 +56,7 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   admin_password        = "AdminPassword1234!"
   disable_password_authentication = false
 
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+  network_interface_ids = [azurerm_network_interface.nic[each.key].id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -58,8 +65,8 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
 
   source_image_reference {
     publisher = "canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts-gen2"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 }
